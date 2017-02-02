ac65ccb13d2aa38c6b6966a657038ac391f01b3b
carllindquist <clindqui@ucsc.edu>
2017-02-01 23:18:51 -0800

    Turbidity Added   tanks now contain turbidity   moveWater() now shifts turbidity of sink tank based on weighted average with sourceTurbidity   new — printTurbidities() prints the turbidity levels of each tank
    Cleaned machine.c code
      Fixed a few comments
      changed all instances of ‘from tank’ to ‘source’
      changed all instances of ‘to tank’ to ‘sink

--- Machine_Simulator/machine.c
+++ Machine_Simulator/machine.c
@@ -23,21 +23,23 @@
 #define TANK_PRINT_WIDTH 4
 #define TANK_PRINT_HEIGHT 11
 #define MAX_TANK_COUNT 10
-#define SPACING 2
+#define SPACING 3
 
 
 //––––––  Private Types  ––––––//
 typedef struct Tank{
 	int volume;
 	int quantity;
+	float turbidity;
 } Tank;
 
 typedef struct Device{
 	int enable;
 	int flowRate;
 	int consumption;
-	Tank* from;
-	Tank* to;
+	float newTurbidity;
+	Tank* source;
+	Tank* sink;
 } Device;
 
 
@@ -49,13 +51,14 @@ int numTanks;
 
 
 //––––––  Private Declarations  ––––––//
-Tank* tank(int volume, int quantity);
-Device* device(int enable, int flowRate, int consumption, Tank* from, Tank* to);
+Tank* tank(int volume, int quantity, float turbidity);
+Device* device(int enable, int flowRate, int consumption, float newTurbidity, Tank* source, Tank* sink);
 void updateMachine(void);
-int moveWater(Tank* from, Tank* to, int amount);
+int moveWater(Tank* source, Tank* sink, int amount, float sourceTurbidity);
 void updateDevice(Device* device);
-void printTanks(Tank* tank[]);
+void printTanks(Tank* tankArr[MAX_TANK_COUNT]);
 void stageTank(char tankStage[][TANK_PRINT_WIDTH], Tank* tank);
+void printTurbidities(Tank* tank[MAX_TANK_COUNT]);
 int isFull(Tank* tank);
 void delay(double dly);
 
@@ -69,6 +72,7 @@ void runMachine(int cycles, float timePerCycle, int graphicsEnable) {
 		if(graphicsEnable) {
 			system("clear");
 			printTanks(tanks);
+			printTurbidities(tanks);
 		}
 		delay(timePerCycle); //Waste time so the user can watch the tanks change
 	}
@@ -78,20 +82,19 @@ void runMachine(int cycles, float timePerCycle, int graphicsEnable) {
 
 /*
 [desc]	Initializes a machine described in two storage arrays. Not yet public
-		due to its relative uselessness as a public function due to low-modularity
-		of machine implementation.
+		due to the low-modularity of machine implementation.
 
 [tankArr] Array containing Tank objects, size must be MAX_TANK_COUNT
 [deviceArr]	Array containing Device objects, size must be MAX_TANK_COUNT			   
 */
 void machineInit(Tank* tankArr[MAX_TANK_COUNT], Device* deviceArr[MAX_TANK_COUNT]) {
 	int i;
-	for (i=0; i < MAX_TANK_COUNT; i++)
+	for (i=0; i < MAX_TANK_COUNT; i++) //Copy to global
 		tanks[i] = tankArr[i];
 	for (i=0; i < MAX_TANK_COUNT; i++)
 		devices[i] = deviceArr[i];
 
-	for (i=0; tanks[i] != NULL; i++);
+	for (i=0; tanks[i] != NULL; i++); //Counts for bookeeping
 	numTanks = i;
 	for (i=0; devices[i] != NULL; i++);
 	numDevices = i;
@@ -100,11 +103,12 @@ void machineInit(Tank* tankArr[MAX_TANK_COUNT], Device* deviceArr[MAX_TANK_COUNT
 
 void defaultMachineInit(void) {
 	//Initialized outside of array for readable device initialization
-	Tank* source = tank(INFINITY, INFINITY);
-	Tank* tank2 = tank(100, 0);
-	Tank* tank3 = tank(100, 0);
-	Tank* tank4 = tank(100, 0);
-	Tank* sink = tank(INFINITY, 0);
+	/* tank(volume, quantity, turbidity) */
+	Tank* source = tank(INFINITY, INFINITY, 5.0);
+	Tank* tank2 = tank(100, 0, 0.0);
+	Tank* tank3 = tank(100, 0, 0.0);
+	Tank* tank4 = tank(100, 0, 0.0);
+	Tank* sink = tank(INFINITY, 0, 0.0);
 
 	Tank* tankArr[MAX_TANK_COUNT] = {};
 	tankArr[0] = source;
@@ -114,15 +118,15 @@ void defaultMachineInit(void) {
 	tankArr[4] = sink;
 
 	Device* deviceArr[MAX_TANK_COUNT] = {};
-	/* device(enable, flowRate, powerconsumption, source, sink) */
+	/* device(enable, flowRate, powerconsumption, newTurbidity, source, sink) */
 	//Filter pump
-	deviceArr[0] = device(FALSE, 15, 10, source, tank2);
+	deviceArr[0] = device(FALSE, 14, 10, 3.0, source, tank2);
 	//RO pump
-	deviceArr[1] = device(FALSE, 9, 30, tank2, tank3);
+	deviceArr[1] = device(FALSE, 5, 9, 0.5, tank2, tank3);
 	//UV disinfect
-	deviceArr[2] = device(FALSE, 20, 15, tank3, tank4);
+	deviceArr[2] = device(FALSE, 20, 10, -1, tank3, tank4);
 	//Slow drain
-	deviceArr[3] = device(FALSE, 3, 15, tank4, sink);
+	deviceArr[3] = device(FALSE, 3, 15, -1, tank4, sink);
 
 	
 	machineInit(tankArr, deviceArr); //Load tanks and devices into global arrays
@@ -139,11 +143,13 @@ void defaultMachineInit(void) {
 
 [volume] Int representing the maximum capacity of the tank.
 [quantity] Int representing how much water is currently in the tank.
+[turbidity] Float representing the tank's turbidity.
 
 [ret]	A pointer to the newly created tank object.	   
 */
-Tank* tank(int volume, int quantity) {
+Tank* tank(int volume, int quantity, float turbidity) {
     Tank* t = (Tank*) malloc(sizeof(Tank));
+    t->turbidity = turbidity;
     t->volume = volume;
     t->quantity = quantity;
 	return t;
@@ -151,20 +157,25 @@ Tank* tank(int volume, int quantity) {
 
 /*
 [desc]	Constructor for a Device object. Devices are designed to move water
-		between tanks.
+		between tanks. Only moves water when they are on.
 
-[volume] Int representing the maximum capacity of the tank.
-[quantity] Int representing how much water is currently in the tank.
+[enable] Decides whether or not the device moves water each cycle.
+[flowRate] Water to be moved by the device per cycle if enable = 1.
+[consumpion] Power consumed by the device (not yet implemented).
+[newTurbidity] Maximum turbidity of water flowing out of this device.
+[source] Tank to source from.
+[sink] Tank to sink into.
 
-[ret]	A pointer to the newly created tank object.	   
+[ret]	A pointer to the newly created device object.	   
 */
-Device* device(int enable, int flowRate, int consumption, Tank* from, Tank* to) {
+Device* device(int enable, int flowRate, int consumption, float newTurbidity, Tank* source, Tank* sink) {
 	Device* d = (Device*) malloc(sizeof(Device));
 	d->enable = enable;
 	d->flowRate = flowRate;
 	d->consumption = consumption;
-	d->from = from;
-	d->to = to;
+	d->newTurbidity = newTurbidity;
+	d->source = source;
+	d->sink = sink;
 	return d;
 }
 
@@ -176,42 +187,55 @@ Device* device(int enable, int flowRate, int consumption, Tank* from, Tank* to) 
 */
 void updateMachine(void) {
 	int i;
-	for(i=0; i < numDevices; i++) {
-		if(devices[i]->enable)
-			moveWater(devices[i]->from, devices[i]->to, devices[i]->flowRate);
+	float sourceTurbidity;
+	for (i=0; i < numDevices; i++) {
+		if (devices[i]->enable) {
+			if (devices[i]->newTurbidity < devices[i]->source->turbidity && devices[i]->newTurbidity >= 0.0)
+				sourceTurbidity = devices[i]->newTurbidity;
+			else
+				sourceTurbidity = devices[i]->source->turbidity;
+			// sourceTurbidity =  ? devices[i]->newTurbidity : devices[i]->source->turbidity;
+			moveWater(devices[i]->source, devices[i]->sink, devices[i]->flowRate, sourceTurbidity);
+		}
 		updateDevice(devices[i]);
 	}
 }
 
 /*
 [desc]	Moves water from one tank into another. Only moves water if the
-		water is availble in the 'from tank'. Will not overfill the 'to tank'.
-			Water movement: 'from tank' -> 'to tank'
+		water is availble in the 'source tank'. Will not overfill the 'sink tank'.
+			Water movement: 'source tank' -> 'sink tank'
 
-[from] Tank to source water from.
-[to] Tank to sink water into.
+[source] Tank to source water from.
+[sink] Tank to sink water into.
 [amount] Amount of water to move.
+[sourceTurbidity] Turbidity of the incoming water.
 
-[ret]	0 if 'to tank' was filled, 1 otherwise.
+[ret]	0 if 'sink tank' was filled, 1 otherwise.
 */
-int moveWater(Tank* from, Tank* to, int amount) {
+int moveWater(Tank* source, Tank* sink, int amount, float sourceTurbidity) {
 	/* increase sink quantity */
-	if (to->volume != INFINITY) { 
-		if (from->quantity < amount)
-			to->quantity += from->quantity; //source doesn't have enough
+	if (sink->volume != INFINITY) { 
+		if (source->quantity < amount)
+			sink->quantity += source->quantity; //source doesn't have enough
 		else 
-			to->quantity += amount; //source has enough
-		if (to->quantity > to->volume)
-			to->quantity = to->volume; //Cap volume at full tank
+			sink->quantity += amount; //source has enough
+		if (sink->quantity > sink->volume)
+			sink->quantity = sink->volume; //Cap volume at full tank
 	}
 
 	/* decrease source tank */
-	if (from->quantity != INFINITY && from->quantity > 0) {
-		from->quantity -= amount;
-		if (from->quantity < 0)
-			from->quantity = 0; //Cap volume at empty tank
+	if (source->quantity != INFINITY && source->quantity > 0) {
+		source->quantity -= amount;
+		if (source->quantity < 0)
+			source->quantity = 0; //Cap volume at empty tank
 	}
-	return isFull(to)? FALSE: TRUE;
+
+	/* set new turbidity for sink using weighted average */
+	sink->turbidity = ( (sourceTurbidity*(float)amount) + (sink->turbidity*sink->quantity) ) /
+		(sink->quantity + (float)amount);
+
+	return isFull(sink)? FALSE: TRUE;
 }
 
 /*
@@ -220,9 +244,9 @@ int moveWater(Tank* from, Tank* to, int amount) {
 [device] Device to update.
 */
 void updateDevice(Device* device) {
-	if (device->to->quantity == device->to->volume)
+	if (device->sink->quantity == device->sink->volume)
 		device->enable = FALSE;
-	else if (device->to->quantity <= device->to->volume/2)
+	else if (device->sink->quantity <= device->sink->volume/2)
 		device->enable = TRUE;
 }
 
@@ -270,7 +294,7 @@ void printTanks(Tank* tanks[MAX_TANK_COUNT]) {
 	int tanksPrinted = 0;
 
 	int numTanks;
-	for (i=0; tanks[i] != NULL; i++);
+	for (i=0; tanks[i] != NULL && i < MAX_TANK_COUNT; i++);
 	numTanks = i;
 	
 	//transfer letters to the line stage
@@ -320,6 +344,16 @@ void stageTank(char tankStage[][TANK_PRINT_WIDTH], Tank* tank) {
 				tankStage[i][j] = ' ';
 		}
 	}
+}
+
+void printTurbidities(Tank* tankArr[MAX_TANK_COUNT]) {
+	// int numTanks;
+	int i;
+	for (i=0; tankArr[i] != NULL && i < MAX_TANK_COUNT; i++) {
+		printf("%04.2f   ", tankArr[i]->turbidity);
+	}
+	// numTanks = i;
+	printf("\n\r");
 }
 
 
