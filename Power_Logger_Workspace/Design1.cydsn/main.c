364e2e7479c04d7e65fd5b1b7e3dec96b0672c54
Carl Lindquist <clindqui@ucsc.edu>
2017-02-09 21:05:12 -0800

    ADC set up   ADC now reads constantly (just for printing purposes), displays voltage to LCD   ADC can only read to a MAX of 5V difference or else you'll break the board

--- Power_Logger_Workspace/Design1.cydsn/main.c
+++ Power_Logger_Workspace/Design1.cydsn/main.c
@@ -10,17 +10,55 @@
  * ========================================
 */
 #include "project.h"
+#include <stdio.h>
+
+#define ADC_RANGE 6.144
+#define ADC_RESOLUTION 4095
+
+CY_ISR_PROTO(Minute_Timer_ISR);
+
+
+
+uint32 adcVal;
+uint8 minTimerFlag;
+
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     LCD_Start();
-    LCD_PrintString("Hello World");
-    for(;;)
-    {
-        /* Place your application code here. */
+    ADC_DelSig_Start();
+    Minute_Timer_Start();
+    Minute_Timer_Interrupt_StartEx(Minute_Timer_ISR);
+    
+    adcVal = 0;
+    double volts = 0;
+    minTimerFlag = 0;
+    
+    char outstring[16] = {};
+    while(1) {
+        adcVal = ADC_DelSig_Read16();
+        if (adcVal > 0x0FFF) { //Cap ADC
+            adcVal = 0x0FFF;
+        }
+        
+        volts = ((float)adcVal / ADC_RESOLUTION) * ADC_RANGE * 2;
+        sprintf(outstring, "Volts: %.1f", volts);
+        LCD_ClearDisplay();
+        LCD_PrintString(outstring);
+
+        if (minTimerFlag) {
+            //Store result in SD Card
+            minTimerFlag = 0;
+        }
+        CyDelay(50);
     }
 }
 
-/* [] END OF FILE */
+CY_ISR(Minute_Timer_ISR) {
+    Minute_Timer_STATUS; //Clears Isr
+    minTimerFlag = 1;
+}
+
+/* EOF */
