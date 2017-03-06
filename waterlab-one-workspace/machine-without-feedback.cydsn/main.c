7e2e0ca48ceb3672435dca650dbd9e22a2bd575b
carllindquist <clindqui@ucsc.edu>
2017-03-05 21:22:21 -0800

    Cleaned main.c

--- waterlab-one-workspace/machine-without-feedback.cydsn/main.c
+++ waterlab-one-workspace/machine-without-feedback.cydsn/main.c
@@ -29,7 +29,7 @@ int main(void) {
     LCD_PrintString("Hello World");
     
     tankInit();
-    machineState = STATE_IDLE;
+    machineState = STATE_IDLE; //IDLE is used as the 'next state' picker
     
     tankStruct tankStates = tankGetStates();
     char outString[30] = {};
@@ -46,6 +46,9 @@ int main(void) {
                 sprintf(outString, "0:%d 1:%d 2:%d 3:%d", tankStates.tank[0], tankStates.tank[1] , tankStates.tank[2], tankStates.tank[3]);
                 LCD_Position(1, 0);
                 LCD_PrintString(outString);
+
+                LED4_Pin_Write(TRUE);
+                LED3_Pin_Write(TRUE);
                 
                 tankStates = tankGetStates();
                 if ( (tankStates.tank[0] == TANK_STATE_MID || tankStates.tank[0] == TANK_STATE_FULL) 
@@ -55,7 +58,6 @@ int main(void) {
                             && tankStates.tank[0] != TANK_STATE_FULL) {
                     machineState = STATE_RUN_P1;
                 }
-                CyDelay(50);
                 break;
             
             case STATE_RUN_P0:
@@ -66,13 +68,8 @@ int main(void) {
                 LCD_PrintString("RUN PUMP 0");
                 LCD_Position(1, 0);
                 LCD_PrintHexUint16(tankEvents);
-
-                
-                LED4_Pin_Write(TRUE);
-                LED3_Pin_Write(FALSE);
                 
                 if (tankEventOccured(TANK_EVENT_0_EMPTY) || tankEventOccured(TANK_EVENT_1_FULL)) {
-                    
                     machineState = STATE_IDLE;
                     tankEvents = TANK_EVENT_NONE;
                 }
@@ -86,23 +83,18 @@ int main(void) {
                 LCD_PrintString("RUN PUMP 1");
                 LCD_Position(1, 0);
                 LCD_PrintHexUint16(tankEvents);
-
-                
-                LED4_Pin_Write(FALSE);
-                LED3_Pin_Write(TRUE);
                 
                 if (tankEventOccured(TANK_EVENT_1_EMPTY) || tankEventOccured(TANK_EVENT_0_FULL)) {
-                    
                     machineState = STATE_IDLE;
                     tankEvents = TANK_EVENT_NONE;
                 }
                 break;
         }
-        CyDelay(100);
+        CyDelay(100); //For LCD printing purposes
     }
     
     
     for(;;); /* Just in case */
 }
 
-/* [] END OF FILE */
+/* EOF */
