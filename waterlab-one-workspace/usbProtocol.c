b97976f18087329f93ce7c83e0d639a14289ff6b
Carl Lindquist <clindqui@ucsc.edu>
2017-03-17 20:39:05 -0700

    usbProtocol library added   Useful for debugging and shells!

---
+++ waterlab-one-workspace/usbProtocol.c
@@ -0,0 +1,51 @@
+/*
+    Carl Lindquist
+    Nov 15, 2016
+    
+    Control code to communicate over USBUART on the PSOC5 LP
+*/
+
+
+#include <project.h>
+#include "usbProtocol.h"
+
+#define MAX_USB_STRING_LENGTH 63
+
+
+//––––––  Private Declarations  ––––––//
+
+void usbStart(void);
+uint8 usbGetByte(void);
+
+//––––––––––––––––––––––––––––––  Public Functions  ––––––––––––––––––––––––––––––//
+
+void usbStart(void) {
+    CyGlobalIntEnable; //Enable PSOC 5LP interrupts
+    USBUART_Start(0u, USBUART_5V_OPERATION);
+    while (!USBUART_GetConfiguration()){};
+    USBUART_CDC_Init(); 
+}
+
+uint8 usbGetByte(void) {
+    uint8 byte = '\0';
+    if (USBUART_DataIsReady()) {
+        USBUART_GetAll(&byte);
+    }
+    return byte;
+}
+
+void usbSendByte(uint8 byte) {
+    while(!USBUART_CDCIsReady());
+    USBUART_PutChar(byte);
+}
+
+void usbSendString(char string[]) {
+    uint8 length = 0;
+    while(string[++length]);
+    if(string[0] && length <= MAX_USB_STRING_LENGTH) {
+        while(!USBUART_CDCIsReady());
+        USBUART_PutData((uint8*)string, length);
+    }
+}
+
+//EOF
---
+++ waterlab-one-workspace/usbProtocol.h
@@ -0,0 +1,45 @@
+/* 
+    Carl Lindquist
+    Nov 15, 2016
+    
+    Interface to communicate over USBUART on the PSOC5 LP
+*/
+
+#ifndef USB_PROTOCOL_H
+#define USB_PROTOCOL_H
+    
+#include <project.h>
+    
+
+/*
+[desc]  Initializes USB, blocks until USB is ready.
+		COM port must be connected for this function to finish
+*/
+void usbStart(void);
+    
+
+/*
+[desc]  Returns the byte sent from the host PC over USBUART.
+
+[ret]	Returns one byte of data if available, '\0' otherwise.
+*/
+uint8 usbGetByte(void);
+
+
+/*
+[desc]  Sends a byte to the host PC over USBUART.
+
+[byte] Byte to be sent
+*/
+void usbSendByte(uint8 byte);
+
+
+/*
+[desc]  Sends a string to the host PC over USBUART. String not sent if
+		length is greater than 63
+
+[string] String to be sent. Length should be less than 64.
+*/
+void usbSendString(char string[]);
+    
+#endif //USB_PROTOCOL_H
